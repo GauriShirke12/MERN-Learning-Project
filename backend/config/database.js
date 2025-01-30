6db582716a7d796695dad56522d227a83c9c5685
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-01-30 12:32:31 +0530

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://gaurishirke2005:gaurishirke123@cluster0.hkgqo.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
    
 }
