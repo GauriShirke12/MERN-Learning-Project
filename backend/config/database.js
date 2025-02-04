afd64b18975ee9d488e9088002307fb23570c23f
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-02-04 10:45:20 +0530

    add-styling

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// Load environment variables from .env
+// Load environment variables
 dotenv.config();
 
 export const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
     if (!mongoURI) {
-      console.error("❌ MongoDB URI is missing in .env file");
+      console.error("MongoDB URI is missing in .env file");
       process.exit(1); // Exit if MongoDB URI is not found
     }
 
@@ -17,9 +17,9 @@ export const connectDB = async () => {
       useUnifiedTopology: true,
     });
 
-    console.log("✅ MongoDB connected successfully");
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
+    console.error(" MongoDB connection error:", error);
     process.exit(1); // Exit on failure
   }
 };
